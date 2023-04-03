36ca3625e3e4ca12655f657d03950273f1e1bfbd
Ayush Kumar <101060011+asyncAyush@users.noreply.github.com>
2023-04-03 10:31:13 -0400

    adding getter and setter function .sol

---
+++ getter and setter function .sol
@@ -0,0 +1,17 @@
+//SPDX licence identifier GPL- 3.0;
+
+ pragma solidity >= 0.5.0 < 0.9.0;
+
+ contract state 
+ {
+     uint age = 20;
+     function gatter() public view returns(uint)
+     {
+         return age;
+
+     }
+     function setter(uint newAge) public 
+     {
+         age = newAge;
+     }
+ }
