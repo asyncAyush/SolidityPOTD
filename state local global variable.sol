45e920210648d139f94674012acfee26f647be0e
Ayush Kumar <101060011+asyncAyush@users.noreply.github.com>
2023-04-11 07:00:15 -0400

    Create state local global variable.sol

---
+++ state local global variable.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract state {
+    uint public num = 8;
+    string public name = "ayush";
+
+    function get() public {
+        uint i =5;
+
+        uint TimeStamp = block.timestamp;
+    }
+}
