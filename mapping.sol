7c5902760e962c97362669cb0727726d44743b71
Ayush Kumar <101060011+asyncAyush@users.noreply.github.com>
2023-04-24 08:44:04 -0400

    Create mapping.sol

---
+++ mapping.sol
@@ -0,0 +1,12 @@
+// SPDX license identifier GPL;
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract demo
+{
+    mapping(uint => string) public roll_no;
+    function setter(uint keys, string memory value) public
+    {
+        roll_no[keys] = value; 
+    }
+}
