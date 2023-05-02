facf67b91dbb4ea1858933e30940b0ad7fd8d955
Ayush Kumar <101060011+asyncAyush@users.noreply.github.com>
2023-05-02 11:19:37 -0400

    Create event organization.sol

---
+++ event organization.sol
@@ -0,0 +1,41 @@
+pragma solidity >= 0.3.0 < 0.9.0;
+
+contract EventContract 
+{
+    struct Event{
+        address organizer;
+        string name;
+        uint date;
+        uint price;
+        uint ticketCount;
+        uint ticketRemain;
+    }
+    mapping (uint => Event) public events;
+    mapping (address => mapping (uint => uint )) public tickets;
+    uint public nextId;
+    function creatEvent (string memory name , uint date , uint price, uint ticketCount) external{
+        require(date>block.timestamp, "you can organize event for future date");
+        require(ticketCount>0, "you can organize event only if you create more then 0 ticket");
+        
+        events[nextId] = Event (msg.sender, name, date, price, ticketCount, ticketCount);
+        nextId ++;
+        }
+        function buyTicket(uint id, uint quantity) external payable{
+            require(events[id].date!=0,"Event does not exist");
+            require (events[id].date>block.timestamp,"Event has alrady occured");
+            Event storage _event = events[id];
+            require(msg.value ==(_event.price*quantity), "Ethere is not enough");
+            require(_event.ticketRemain>=quantity, "not enough ticket");
+            _event.ticketRemain-=quantity;
+            tickets[msg.sender][id]+=quantity;
+    }
+    function transferTicket(uint id, uint quantity, address to) external{
+         require(events[id].date !=0, "Event does not exist");
+        require (events[id].date>block.timestamp,"Event has alrady occured");
+        require (tickets[msg.sender][id]>quantity, "you do not enough ticket");
+        tickets[msg.sender][id]=quantity;
+        tickets[to][id]+=quantity;
+    }
+
+    
+}
