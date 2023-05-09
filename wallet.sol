4cef67a72ae30fd0ddd9d5d09250cb375a1563f8
Ayush Kumar <101060011+asyncAyush@users.noreply.github.com>
2023-05-09 09:39:48 -0400

    Create wallet.sol

---
+++ wallet.sol
@@ -0,0 +1,28 @@
+// spdx- license-identifire: GPL-3.0;
+
+pragma solidity ^0.8.17;
+
+contract wallet {
+    string public name = "wallet";
+    uint num;
+
+    function setvalue (uint _num ) public {
+        num =_num;
+    }
+    function getValue() public view returns(uint){
+        return num;
+    }
+    function sendEthContract()public payable{
+
+    }
+    function contractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+    function sendEthUser(address _user) public payable{
+        payable(_user).transfer(msg.value);
+    }
+    function accountBalance(address _address) public view returns(uint){
+        return (_address).balance;
+    }
+}
+//0x0fc6b46e4836bef4d5dd4982ed30f173ea0df7b2
